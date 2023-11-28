00b3ca807cb57b5fc91c2dfa0b85f191ebd4c4b8
sana7867 <147515885+sana7867@users.noreply.github.com>
2023-11-28 16:49:13 +0100

    Add files via upload

---
+++ Pharmacie/src/main/java/ma/project/PharmacieApplication.java
@@ -0,0 +1,13 @@
+package ma.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacieApplication.class, args);
+	}
+
+}
---
+++ Pharmacie/src/main/java/ma/project/entities/Zone.java
@@ -0,0 +1,40 @@
+package ma.project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Zone {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	private String image;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Zone() {
+		super();
+	}
+	 
+	
+}
