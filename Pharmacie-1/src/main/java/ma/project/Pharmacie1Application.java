025ce5fdf83e149aa3c122ac8ab214677cc20122
sana7867 <147515885+sana7867@users.noreply.github.com>
2023-11-28 17:16:20 +0100

    Add files via upload

---
+++ Pharmacie-1/src/main/java/ma/project/Pharmacie1Application.java
@@ -0,0 +1,13 @@
+package ma.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pharmacie1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pharmacie1Application.class, args);
+	}
+
+}
