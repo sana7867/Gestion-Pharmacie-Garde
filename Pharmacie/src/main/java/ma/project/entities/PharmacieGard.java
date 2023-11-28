62740f88f8283a65f17400f2d0f5ab970761888d
khadija23362 <147508009+khadija23362@users.noreply.github.com>
2023-11-28 17:14:54 +0100

    garde

---
+++ Pharmacie/src/main/java/ma/project/entities/PharmacieGard.java
@@ -0,0 +1,56 @@
+package ma.project.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class PharmacieGard  implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Temporal(TemporalType.DATE)
+	private Date dateDubut;
+	@Temporal(TemporalType.DATE)
+	private Date dateFin;
+	@ManyToAny
+	private Garde gard;
+	
+	public PharmacieGard() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getDateDubut() {
+		return dateDubut;
+	}
+	public void setDateDubut(Date dateDubut) {
+		this.dateDubut = dateDubut;
+	}
+	public Date getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	public Garde getGard() {
+		return gard;
+	}
+	public void setGard(Garde gard) {
+		this.gard = gard;
+	}
+
+	
+}
